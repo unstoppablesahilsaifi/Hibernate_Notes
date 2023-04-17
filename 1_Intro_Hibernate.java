63262ebc5ddcc60efbd48a89cb5bd7f57af98de7
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2023-04-17 22:46:33 +0530

    Update 1_Intro_Hibernate.java

--- 1_Intro_Hibernate.java
+++ 1_Intro_Hibernate.java
@@ -1,1 +1,12 @@
-Test
+Hibernate-
+          It is a Java framework that simplifies the development of java application to interact with the database.
+
+ORM- Object Relational Modal
+
+NOTE: We map our table to class, Class behaves like a table and class fields are like column names.
+  
+After creating the maven project, we need to add dependecies in POM.XML file.
+1. Add hibernate dependecy in pom.xml
+2. Add MySQL connector dependency with the same version as MySQL installed version.
+  
+  
